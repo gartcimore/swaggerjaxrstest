468ff90e8350c1cea5b0a58dfd5caabd9f16292a
gartcimore <gart.algar@gmail.com>
2017-03-24 11:33:09 +0100

    added a test that send null entity

---
+++ src/test/java/org/test/EchoTest.java
@@ -0,0 +1,35 @@
+package org.test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import org.junit.Test;
+import org.restlet.Client;
+import org.restlet.Context;
+import org.restlet.data.MediaType;
+import org.restlet.ext.jackson.JacksonRepresentation;
+import org.restlet.resource.ClientResource;
+
+import java.io.IOException;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.restlet.data.Protocol.HTTP;
+
+public class EchoTest {
+
+
+  @Test
+  public void shouldNotHaveNullPointer() throws IOException {
+    Client client = new Client(new Context(), HTTP);
+    ClientResource clientResource = new ClientResource("http://localhost:8283/echo");
+    clientResource.setNext(client);
+    clientResource.accept(MediaType.APPLICATION_JSON);
+//    clientResource.put(new JacksonRepresentation<>((JsonNode) null));
+    clientResource.put(null);
+    assertThat(clientResource.getResponseEntity().getText(), is(nullValue()));
+    assertThat(clientResource.getStatus().getCode(), is(204));
+  }
+
+}
+
