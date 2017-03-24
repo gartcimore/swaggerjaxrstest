1e018addef6c1699a2b4dfd6a81a3bf85db913c2
gartcimore <gart.algar@gmail.com>
2017-03-24 11:34:21 +0100

    added simple formating to error handling

--- src/main/java/org/test/CustomExceptionMapper.java
+++ src/main/java/org/test/CustomExceptionMapper.java
@@ -7,9 +7,18 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class CustomExceptionMapper implements ExceptionMapper<Exception> {
 
+  private static final String ERROR_CODE = "code";
+  private static final String DESCRIPTION = "description";
+  private static final String TITLE = "title";
+
+  public static final String ERROR_FORMAT = "{\"" + ERROR_CODE + "\":%d,\"" + TITLE + "\":\"%s\",\"" + DESCRIPTION + "\":\"%s\"}";
+
+  private static final String FATAL_ERROR = "Fatal error";
   @Override
   public Response toResponse(Exception exception) {
-
-    return Response.status(500).entity(String.format("error occured : %s", exception.getMessage())).type("application/json").build();
+    System.out.println("error is "+exception.getMessage());
+    exception.printStackTrace();
+    return Response.status(500).entity(String.format(ERROR_FORMAT, 1337, FATAL_ERROR,
+      String.format("Unexpected error: %s", exception.getMessage()))).type("application/json").build();
   }
 }
