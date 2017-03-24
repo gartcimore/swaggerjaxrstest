32ac7b7bdc8e4d6981cb9c4b486583fdb1bf4abb
gartcimore <gart.algar@gmail.com>
2017-03-24 11:40:45 +0100

    added almost same exception mapper provider

---
+++ src/main/java/org/test/BaseExceptionMapper.java
@@ -0,0 +1,27 @@
+package org.test;
+
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class BaseExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<Exception> {
+
+  private static final String ERROR_CODE = "code";
+  private static final String DESCRIPTION = "description";
+  private static final String TITLE = "title";
+
+  public static final String ERROR_FORMAT = "{\"" + ERROR_CODE + "\":%d,\"" + TITLE + "\":\"%s\",\"" + DESCRIPTION + "\":\"%s\"}";
+
+
+  private static final String FATAL_ERROR = "Fatal error";
+
+  @Override
+  public Response toResponse(Exception exception) {
+
+    return Response.status(500).entity(String.format(ERROR_FORMAT, 1337, FATAL_ERROR,
+      String.format("Unexpected error: %s", exception.getMessage()))).type("application/json").build();
+  }
+
+
+}
