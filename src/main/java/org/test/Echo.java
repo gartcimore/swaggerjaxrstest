ab28687725bcae2d82bb38f2d17bf84e3d88df55
gartcimore <gart.algar@gmail.com>
2017-03-24 11:40:01 +0100

    added Post method

--- src/main/java/org/test/Echo.java
+++ src/main/java/org/test/Echo.java
@@ -3,18 +3,36 @@ package org.test;
 import org.restlet.resource.Get;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 
 @Path(value = "/echo")
 public class Echo {
 
 
   @PUT
-  @Consumes({"text/plain", "application/json"})
-  @Produces({"application/json; charset=UTF-8"})
-  public String process(String request) {
+  @Consumes({"application/json"})
+  @Produces({"application/json"})
+  public String process(String request, @Context UriInfo info) throws Exception {
+        System.out.println(info);
+    if (request != null && request.contains("error")) {
+      throw new Exception("error occured ");
+    }
+    return request;
+
+  }
+
+  @POST
+  @Consumes({"application/json"})
+  @Produces({"application/json"})
+  public String process(String request) throws Exception {
+    if (request != null && request.contains("error")) {
+      throw new Exception("error occured ");
+    }
     return request;
 
   }
